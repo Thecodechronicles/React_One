bdb51b8a4cef3660d4b6212918b703c0ee1b5c20
Ankit Mittal <ankitmittal524@gmail.com>
2023-09-16 14:30:35 +0530

    useRef removed

--- src/components/addCart.js
+++ src/components/addCart.js
@@ -1,18 +1,10 @@
-import { useRef } from 'react'
 import { useDispatch } from "react-redux"
 import { addToCart } from '../actions/cartActions.js';
 
 export default (props) => {
     const dispatch = useDispatch();
-    const persistObject = useRef();
-
-
-    if (persistObject.current == null) {
-        persistObject.current = 0;
-    }
 
     const additionToCart = () => {
-        persistObject.current = persistObject.current + 1;
         fetch(`https://dummyjson.com/products/${props.id}`).then((response) => {
             response.json().then((data) => {
                 dispatch(addToCart(data.title));
